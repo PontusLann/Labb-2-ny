50daba6895de2d33a60d77e999761ce46fdbc477
PontusLann <pontus.lann@iths.se>
2022-10-07 13:27:04 +0200

    helt klar

--- Labb 2 ny/Program.cs
+++ Labb 2 ny/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Numerics;
+using Shapes;
+using static Shapes.Shape;
+
+Shape[] shapes = new Shape[20];
+
+
+for (int i = 0; i < shapes.Length; i++)
+{
+    shapes[i] = Shape.GenerateShape();
+    Console.WriteLine(shapes[i]);
+}
+Console.WriteLine();
+Shape.PrintValues();
+
